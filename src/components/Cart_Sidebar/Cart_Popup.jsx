79d9c7c8e06a4f2a33ceaf80cabb807a19532c12
Rahmat-Khavari-Hyper-Island <145808562+Rahmat-Khavari-Hyper-Island@users.noreply.github.com>
2024-02-21 21:43:04 +0100

    upload

--- src/components/Cart_Sidebar/Cart_Popup.jsx
+++ src/components/Cart_Sidebar/Cart_Popup.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { usePopup } from '../../providers/useCartPopup';
-import './Cart_Popup.css';
+import React from "react";
+import { usePopup } from "../../providers/useCartPopup";
+import "./Cart_Popup.css";
 
 const Popup = () => {
   const { isOpen, togglePopup } = usePopup();
 
   return (
-    <div className={`popup ${isOpen ? 'open' : ''}`}>
-      <button className="close-btn" onClick={togglePopup}>Close</button>
+    <div className={`popup ${isOpen ? "open" : ""}`}>
+      <button className="close-btn" onClick={togglePopup}>
+        Close
+      </button>
       <div className="popup-content">
         {/* Add your content here */}
-        <h2>Popup Content</h2>
-        <p>This is a pop-up div on the right side of the page.</p>
+        <h2>Popup Cart Content</h2>
+        <p>This is a pop-up div for cart list.</p>
       </div>
     </div>
   );
