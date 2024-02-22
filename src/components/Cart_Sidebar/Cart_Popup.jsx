50c6198a93b972afefd051919bf53803236df017
Rahmat-Khavari-Hyper-Island <145808562+Rahmat-Khavari-Hyper-Island@users.noreply.github.com>
2024-02-22 20:02:11 +0100

    cart sidebar logic

--- src/components/Cart_Sidebar/Cart_Popup.jsx
+++ src/components/Cart_Sidebar/Cart_Popup.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import { usePopup } from "../../providers/useCartPopup";
+import { ShopProviderContext } from "../../providers/shopProvider";
 import "./Cart_Popup.css";
 
 const Popup = () => {
   const { isOpen, togglePopup } = usePopup();
-
+  const { cart } = useContext(ShopProviderContext);
+  console.log(cart);
   return (
     <div className={`popup ${isOpen ? "open" : ""}`}>
       <button className="close-btn" onClick={togglePopup}>
-        Close
+        X
       </button>
       <div className="popup-content">
-        {/* Add your content here */}
-        <h2>Popup Cart Content</h2>
-        <p>This is a pop-up div for cart list.</p>
+        <div>
+          <h2>Your Cart:</h2>
+        </div>
+        <div>
+          <ul>
+            {cart.map(({ id, name, price, quantity, totalPrice }) => (
+              <li key={id}>
+                {quantity} : {name}= {`${totalPrice} SEK`}{" "}
+                <button>remove</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>total price:</p>
+        </div>
+
+        <div>
+          <button>checkout</button>
+          <button>Clear</button>
+          <button className="close-btn2" onClick={togglePopup}>
+            Close the cart
+          </button>
+        </div>
       </div>
     </div>
   );
