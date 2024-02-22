ddc39b92f6493daad969a35f5847b85840d80f56
Ahmed Sharif <144018452+Sheriff702@users.noreply.github.com>
2024-02-22 12:32:50 +0100

    some changes. added cart button in the product pg

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -2,11 +2,17 @@ import { useParams } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
 import { ShopProviderContext } from "../../providers/ShopProvider";
 import "./ProductPage.css";
+import addToCart from "../../helpers/addToCart";
 
 const ProductPage = () => {
   const { productId } = useParams();
   const { products } = useContext(ShopProviderContext);
   const [product, setProduct] = useState(null);
+  const { cartItems, setCartItems } = useContext(ShopProviderContext);
+  function handleClick() {
+    const newCart = addToCart(product.id, 1, cartItems);
+    setCartItems(newCart);
+  }
 
   useEffect(() => {
     const productDetails = products.find((product) => product.id === productId);
@@ -33,6 +39,7 @@ const ProductPage = () => {
         <div>
           <p>{product.description}</p>
           <p>Price: {product.price}</p>
+          <button onClick={handleClick}>Add to Cart</button>
         </div>
       </div>
     </>
